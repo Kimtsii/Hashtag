0bfb44465484f9bc85567d1ce021dd029f609a3f
Joseph Aurel <joseph@fightclubphilippines.com>
2022-08-04 13:15:14 +0800

    Can check the # of times the hshtag has been used.

--- FacebookApi.go
+++ FacebookApi.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-
-	//"reflect"
+	_ "os"
 	"strings"
 
 	fb "github.com/huandu/facebook/v2"
 	"github.com/mehanizm/airtable"
 )
 
-// You have to set your environment variables properly for either MacOS, Linux, or Windows
-var FbAccessToken string = ("EAAHPmz80hhABALeWt0zTDFOVXNUEPG9noL6DSRC40YxnhDSqK4rTFJ8TTYHS3dTjfXCer1inX7PkaqZAFo8XoZAaexcHQd2YC68xdcCAz5BXzWRh9O6yp9Il97TWwIIiYiRZAGaBrCLCtEJVeC9cTRIwN1z90NOmwjbsloBSZAGTK8Pf8WRD		")
+// type User struct {
+// 	Name string `example:"name"`
+// }
+
+// func (u *User) String() string {
+// 	return fmt.Sprintf("Hi! My name is %s", u.Name)
+// }
+
+var FbAccessToken string = ("EAAHPmz80hhABALeWt0zTDFOVXNUEPG9noL6DSRC40YxnhDSqK4rTFJ8TTYHS3dTjfXCer1inX7PkaqZAFo8XoZAaexcHQd2YC68xdcCAz5BXzWRh9O6yp9Il97TWwIIiYiRZAGaBrCLCtEJVeC9cTRIwN1z90NOmwjbsloBSZAGTK8Pf8WRD				")
 var ATClientToken string = ("keyOmJMHGYoQpMxYw")
 var ATBaseID string = ("appQntnFzrheCxlir")
 
@@ -39,20 +45,19 @@ type FacebookPostShares struct {
 }
 
 type FacebookPostReactions struct {
-	Count   int    `facebook:"total_count"`
-	Message string `facebook:"viewer_reaction"`
+	Count    int    `facebook:"total_count"`
+	Message1 string `facebook:"viewer_reaction"`
 }
 
-func parse_for_hashtags(message string) []string {
+func parse_for_hashtags(Message string) []string {
 	var hashtags []string
 	// var hashtags = make([]string,0,100)
 
-	possibletags := strings.Fields(message) // Split message on spaces
+	possibletags := strings.Fields(Message) // Split message on spaces
 	for _, value := range possibletags {
 		if strings.HasPrefix(value, "#") {
-			//fmt.Println("found hashtag: ", value)
+			fmt.Println("found hashtag: ", value)
 			hashtags = append(hashtags, value)
-
 		}
 	}
 
@@ -74,7 +79,7 @@ func init() {
 func get_latest_fb_post() FacebookPost {
 	var feed FacebookPost
 
-	result, _ := fb.Get("107704657675864/feed?fields=reactions.summary(true),message,likes,created_time,shares,from&limit=2", fb.Params{
+	result, _ := fb.Get("107704657675864_617731970006461?fields=reactions.summary(true),message,likes,created_time,shares,from&limit=2", fb.Params{
 		"access_token": FbAccessToken,
 	})
 	result.DecodeField("data.0", &feed) // Unmarshal the JSON results into feed struct
@@ -85,21 +90,20 @@ func get_latest_fb_post() FacebookPost {
 	feed.MsgHashTags = parse_for_hashtags(feed.Message)
 	//fmt.Println("latest post shares is", feed.FeedFromShares.Count)
 	//fmt.Println("latest post react is", feed.FeedFromReact.Count)\
+
 	return feed
 }
 
-// func contains(s []string, v string) bool {
-// 	for _, s := range s {
-// 		if v == s {
-// 			return true
-// 		}
+// func main() {
+// 	u := &User{
+// 		Name: "Sammy",
 // 	}
-// 	return false
+
+// 	fmt.Println(u)
 // }
 
-//  Really need to test for a whole bunch of different conditions
-
 func main() {
+	//
 
 	{
 		client := airtable.NewClient("keyOmJMHGYoQpMxYw")
@@ -149,7 +153,7 @@ func main() {
 
 									"Hashtag":   v.Fields["Hashtag"],
 									"Last Used": feed.CreatedTime,
-									//	"Count":     "UPDATED",
+									"Count":     v.Fields["Count"],
 								},
 							},
 						},
@@ -178,7 +182,7 @@ func main() {
 							Fields: map[string]interface{}{
 								"Hashtag":   x,
 								"Last Used": feed.CreatedTime,
-								//"Count":     "ADDED",
+								"Count":     +1,
 								//"Record ID":       v.ID,
 								//"Created Time": feed.CreatedTime,
 							},
